32d3575363b842e5c6eb0b0692f6c7a01df1e1e6
DinarDi <bugagafree@gmail.com>
2021-08-25 21:45:45 +0500

    add five day forecast

--- src/store/weather.ts
+++ src/store/weather.ts
@@ -18,8 +18,24 @@ interface IcurrentWeather {
   main: Imain;
 }
 
+interface ImainFive {
+  temp_min: number;
+  temp_max: number;
+}
+
+interface IfiveDayForecast {
+  dt_txt: string;
+  main: ImainFive;
+  weather: Iweather[];
+}
+
+interface IfiveDayForecastAPI {
+  list: IfiveDayForecast[];
+}
+
 class Weather {
   currentWeather: IcurrentWeather[] = [];
+  fiveDayForecast: IfiveDayForecast[] = [];
   loading: boolean = false;
   error: boolean = false;
   errorText: string = "";
@@ -30,19 +46,51 @@ class Weather {
 
   fetchCurrentWeather = async (city: string) => {
     try {
+      this.fiveDayForecast = [];
       this.loading = true;
       this.error = false;
       this.errorText = "";
       const response = await axios.get<IcurrentWeather>(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
       );
+      const responseFive = await axios
+        .get<IfiveDayForecastAPI>(
+          `https://api.openweathermap.org/data/2.5/forecast?q=Moscow&appid=${API_KEY}`,
+          {
+            params: { units: "metric" },
+          }
+        )
+        .then((resp) => resp.data.list);
       this.currentWeather = [response.data];
+      for (let i = 0; i < responseFive.length; i += 8) {
+        this.fiveDayForecast = [...this.fiveDayForecast, responseFive[i]];
+      }
       this.loading = false;
     } catch (e) {
       this.error = true;
       this.errorText = `${e}`;
     }
   };
+
+  // fetchFiveDaysForecast = async (city: string) => {
+  //   try {
+  //     this.fiveDayForecast = [];
+  //     const response = await axios
+  //       .get<IfiveDayForecastAPI>(
+  //         `https://api.openweathermap.org/data/2.5/forecast?q=Moscow&appid=${API_KEY}`,
+  //         {
+  //           params: { units: "metric" },
+  //         }
+  //       )
+  //       .then((resp) => resp.data.list);
+  //     for (let i = 0; i < response.length; i += 8) {
+  //       this.fiveDayForecast = [...this.fiveDayForecast, response[i]];
+  //     }
+  //     console.log(this.fiveDayForecast);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 }
 
 export default new Weather();
